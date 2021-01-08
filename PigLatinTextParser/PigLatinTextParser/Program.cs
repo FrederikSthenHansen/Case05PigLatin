8db01e3100190d2536abd22665f9fd66f63b7ac0
Frederik Sthen Hansen <38318697+FrederikSthenHansen@users.noreply.github.com>
2021-01-08 13:28:14 +0100

    File reading and writing is broken beyond repair. reverting earlier commit

--- PigLatinTextParser/PigLatinTextParser/Program.cs
+++ PigLatinTextParser/PigLatinTextParser/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace PigLatinTextParser
 {
@@ -7,10 +8,24 @@ namespace PigLatinTextParser
         private const string myPlaceHolder = "This is a 'placeholder text', to make my piglatin parser. /r/n It acts as a placeholder for a fully parsed text of some sort /r /n This specific placeholder i 3 lines long";
         static void Main(string[] args)
         {
+            Console.WriteLine(Directory.GetCurrentDirectory());
+
+            //try
+            //{
+            //    //Set the current directory.
+            //    Directory.SetCurrentDirectory(dir);
+            //}
+            //catch (DirectoryNotFoundException e)
+            //{
+            //    Console.WriteLine("The specified directory does not exist. {0}", e);
+            //}
+
+
+
             TextParser myParser = new TextParser();
             TextFileHandler myTextFileHandler = new TextFileHandler();
 
-            const string _inputTextPathProject = @"C:\Users\SA02- Frederik\Documents\Case05PigLatin\PigLatinTextParser\PigLatinTextParser\InputText\InputText.Txt";
+            const string _inputTextPathProject = @"C:\Users\SA02 - Frederik\Documents\Case05PigLatin\PigLatinTextParser\PigLatinTextParser\InputText\Case5.pdf";
             const string _inputTextPathDesktop = @"C:\Users\SA02- Frederik\Desktop\Case5.pdf";
 
 
--- PigLatinTextParser/PigLatinTextParser/TextFileHandler.cs
+++ PigLatinTextParser/PigLatinTextParser/TextFileHandler.cs
@@ -8,7 +8,7 @@ using UglyToad.PdfPig.Content;
 namespace PigLatinTextParser
 {
     class TextFileHandler
-    {   const string outputPath = @"C:\Users\SA02- Frederik\Documents\Case05PigLatin\PigLatinTextParser\PigLatinTextParser\OutputText\OutputText.txt"; 
+    {   const string outputPath = @"\Case05PigLatin\PigLatinTextParser\PigLatinTextParser\OutputText\OutputText.txt"; 
         TextParser myParser = new TextParser();
         
         private string[] RawTextArray = new string[] { "" };
@@ -17,20 +17,24 @@ namespace PigLatinTextParser
 
         private string[] readPDF(string path)
         {
-            
-            using (PdfDocument document = PdfDocument.Open(path))
+            if (File.Exists(path))
             {
-                string[] ret = new string[document.NumberOfPages];
-                for (int page=1; page<document.NumberOfPages;page++)
+                Console.WriteLine("opening "+ path);
+                using (PdfDocument document = PdfDocument.Open(path))
                 {
-                    Page myPage= document.GetPage(page);
-                    string pageText = myPage.Text;
+                    string[] ret = new string[document.NumberOfPages];
+                    for (int page = 1; page < document.NumberOfPages; page++)
+                    {
+                        Page myPage = document.GetPage(page);
+                        string pageText = myPage.Text;
 
-                    ret[page - 1] = pageText;
+                        ret[page - 1] = pageText;
+                    }
+                    return ret;
                 }
-                return ret;
             }
-            
+            Console.WriteLine(path+" does not exist" );
+            return new string[] { "" };
         }
 
         private string[] readTXT(string path)
@@ -41,21 +45,50 @@ namespace PigLatinTextParser
             return ret;
         }
 
+
+        private void setMyDirectory(string path)
+        {
+            try
+            {
+                //Set the current directory.
+                Directory.SetCurrentDirectory(path);
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine("The specified directory does not exist. {0}", e);
+            }
+        }
+
         //Needs Refactoring to accept docx format and other formats
         public string[] ReadFiles(string path)
         {
+
+            //setMyDirectory(Directory.GetDirectoryRoot(Directory.GetCurrentDirectory()));
+            //Console.WriteLine(Directory.GetCurrentDirectory());
+
+
+            //setMyDirectory(@"C:\Users\SA02 - Frederik\Documents\Case05PigLatin\PigLatinTextParser\PigLatinTextParser\InputText");
+            //Console.WriteLine(Directory.GetCurrentDirectory());
+
+
+            string ProperPath =/* Path.GetFullPath(path);*/ path;
+            Console.WriteLine("Path to read file from: "+ ProperPath);
+
             string[] ret = new string[] { "" };
             //TODO: Be Change method to be able to read from PDF files and other file types.
             //TODO: make this method able to read files format from path, and select the right file reading method.
 
+            //Determine filetype to use correct tools to read it
+            string myFileType = Path.GetExtension(path);
+            Console.WriteLine("File has the extension: "+ myFileType );
 
-            if (false) //Make this statement check for file format is txt.
+            if (myFileType==".txt" || myFileType==".TXT") 
             {
-                ret = readTXT(path);
+                ret = readTXT(ProperPath);
             }
-            else if (true) //other file format to be entered here
+            else if (myFileType == ".pdf" || myFileType == ".PDF") 
             {
-                ret = readPDF(path);
+                ret = readPDF(ProperPath);
             }
             return ret;
         }
@@ -89,7 +122,7 @@ namespace PigLatinTextParser
             }
             //the finished text is printed to console
             Console.WriteLine(TreatedText);
-            File.WriteAllText(outputPath, TreatedText);
+            File.WriteAllText(Path.GetFullPath(outputPath), TreatedText);
             //TODO: write finished text to new .TXT file in output folder.
         }
     }
