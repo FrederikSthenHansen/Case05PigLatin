ff5655b168f5a401193e8b1e9c38dd783fad8bec
Frederik Sthen Hansen <38318697+FrederikSthenHansen@users.noreply.github.com>
2021-01-08 13:28:49 +0100

    Revert "File reading and writing is broken beyond repair. reverting earlier commit"
    This reverts commit 8db01e3100190d2536abd22665f9fd66f63b7ac0.

--- PigLatinTextParser/PigLatinTextParser/TextFileHandler.cs
+++ PigLatinTextParser/PigLatinTextParser/TextFileHandler.cs
@@ -8,7 +8,7 @@ using UglyToad.PdfPig.Content;
 namespace PigLatinTextParser
 {
     class TextFileHandler
-    {   const string outputPath = @"\Case05PigLatin\PigLatinTextParser\PigLatinTextParser\OutputText\OutputText.txt"; 
+    {   const string outputPath = @"C:\Users\SA02- Frederik\Documents\Case05PigLatin\PigLatinTextParser\PigLatinTextParser\OutputText\OutputText.txt"; 
         TextParser myParser = new TextParser();
         
         private string[] RawTextArray = new string[] { "" };
@@ -17,24 +17,20 @@ namespace PigLatinTextParser
 
         private string[] readPDF(string path)
         {
-            if (File.Exists(path))
+            
+            using (PdfDocument document = PdfDocument.Open(path))
             {
-                Console.WriteLine("opening "+ path);
-                using (PdfDocument document = PdfDocument.Open(path))
+                string[] ret = new string[document.NumberOfPages];
+                for (int page=1; page<document.NumberOfPages;page++)
                 {
-                    string[] ret = new string[document.NumberOfPages];
-                    for (int page = 1; page < document.NumberOfPages; page++)
-                    {
-                        Page myPage = document.GetPage(page);
-                        string pageText = myPage.Text;
+                    Page myPage= document.GetPage(page);
+                    string pageText = myPage.Text;
 
-                        ret[page - 1] = pageText;
-                    }
-                    return ret;
+                    ret[page - 1] = pageText;
                 }
+                return ret;
             }
-            Console.WriteLine(path+" does not exist" );
-            return new string[] { "" };
+            
         }
 
         private string[] readTXT(string path)
@@ -45,50 +41,21 @@ namespace PigLatinTextParser
             return ret;
         }
 
-
-        private void setMyDirectory(string path)
-        {
-            try
-            {
-                //Set the current directory.
-                Directory.SetCurrentDirectory(path);
-            }
-            catch (DirectoryNotFoundException e)
-            {
-                Console.WriteLine("The specified directory does not exist. {0}", e);
-            }
-        }
-
         //Needs Refactoring to accept docx format and other formats
         public string[] ReadFiles(string path)
         {
-
-            //setMyDirectory(Directory.GetDirectoryRoot(Directory.GetCurrentDirectory()));
-            //Console.WriteLine(Directory.GetCurrentDirectory());
-
-
-            //setMyDirectory(@"C:\Users\SA02 - Frederik\Documents\Case05PigLatin\PigLatinTextParser\PigLatinTextParser\InputText");
-            //Console.WriteLine(Directory.GetCurrentDirectory());
-
-
-            string ProperPath =/* Path.GetFullPath(path);*/ path;
-            Console.WriteLine("Path to read file from: "+ ProperPath);
-
             string[] ret = new string[] { "" };
             //TODO: Be Change method to be able to read from PDF files and other file types.
             //TODO: make this method able to read files format from path, and select the right file reading method.
 
-            //Determine filetype to use correct tools to read it
-            string myFileType = Path.GetExtension(path);
-            Console.WriteLine("File has the extension: "+ myFileType );
 
-            if (myFileType==".txt" || myFileType==".TXT") 
+            if (false) //Make this statement check for file format is txt.
             {
-                ret = readTXT(ProperPath);
+                ret = readTXT(path);
             }
-            else if (myFileType == ".pdf" || myFileType == ".PDF") 
+            else if (true) //other file format to be entered here
             {
-                ret = readPDF(ProperPath);
+                ret = readPDF(path);
             }
             return ret;
         }
@@ -122,7 +89,7 @@ namespace PigLatinTextParser
             }
             //the finished text is printed to console
             Console.WriteLine(TreatedText);
-            File.WriteAllText(Path.GetFullPath(outputPath), TreatedText);
+            File.WriteAllText(outputPath, TreatedText);
             //TODO: write finished text to new .TXT file in output folder.
         }
     }
